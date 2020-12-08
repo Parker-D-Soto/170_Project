ee3c092656a1caefac7cad886f1366f085536709
HHUUUGE <63589860+HHUUUGE@users.noreply.github.com>
2020-12-08 00:50:09 -0800

    Dialogue_Graph_Tool
    Implemented a visual dialogue graph tool for dialogue graph tree implementation (incomplete, needs functions to save and load created graphs)

---
+++ 170_Project/Assets/Dialogue_Graph/Dialogue_Graph.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+//using Subtegral.DialogueSystem.DataContainers;
+
+public class Dialogue_Graph : EditorWindow
+{
+    private Dialogue_Graphview _graphView;
+    [MenuItem("Graph/Dialogue Graph")]
+    public static void OpenDialogueGraphWindow()
+    {
+        var window = GetWindow<Dialogue_Graph>();
+        window.titleContent = new GUIContent(text: "Dialogue Graph");
+    }
+
+    private void OnEnable()
+    {
+        ConstructGraphView();
+        GenerateToolbar();
+    }
+
+    private void ConstructGraphView()
+    {
+        _graphView = new Dialogue_Graphview
+        {
+            name = "Dialogue Graph"
+        };
+
+        _graphView.StretchToParentSize();
+        rootVisualElement.Add(_graphView);
+
+
+    }
+
+    private void GenerateToolbar()
+    {
+        var toolbar = new Toolbar();
+
+        var nodeCreateButton = new Button(clickEvent: () => { _graphView.CreateNode("Dialogue Node");});
+        nodeCreateButton.text = "Create Node";
+
+        toolbar.Add(nodeCreateButton);
+
+        rootVisualElement.Add(toolbar);
+    }
+
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(_graphView);
+    }
+}
---
+++ 170_Project/Assets/Dialogue_Graph/Dialogue_Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_Node : Node
+{
+    public string GUID;
+
+    public string DialogueText;
+
+    public bool EntryPoint = false;
+
+
+}
