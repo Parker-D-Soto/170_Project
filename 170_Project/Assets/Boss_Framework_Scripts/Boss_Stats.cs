61d932cbe8e762813f56d5e7444b082a63d891da
Parker-D-Soto <parkerdsoto@gmail.com>
2021-01-20 02:22:14 -0800

    Created Boss_Stats
    Created a Sprite prefab containing a script for the basic boss statistics and functions to alter the statistics as needed

---
+++ 170_Project/Assets/Boss_Framework_Scripts/Boss_Stats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Stats : MonoBehaviour
+{
+    private int health;
+
+    private int last_attack;
+
+    private bool alive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        last_attack = 0;
+        alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(alive && health <= 0)
+        {
+            alive = false;
+        }
+    }
+
+    public void Minus_Health(int damage)
+    {
+        health = health - damage;
+    }
+
+    public void Change_Last_Attack(int new_attack)
+    {
+        last_attack = new_attack;
+    }
+}
