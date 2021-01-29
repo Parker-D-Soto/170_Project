d55f874deada25541a760d0712b9f0d812c8c522
tijgee <tijgee@ucsc.edu>
2021-01-29 15:00:15 -0800

    adding boss files

---
+++ 170_Project/Assets/Scripts/Boss_Attack_Steady_Aim_Fire/Enemy_Fire.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Fire : MonoBehaviour
+{
+	private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    private Transform player;
+
+	void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+	}
+	
+	void Update() {
+        if (timeBtwShots <= 0){
+             Instantiate(projectile, transform.position, Quaternion.identity);
+             timeBtwShots = startTimeBtwShots;
+        } else {
+            timeBtwShots -= Time.deltaTime;
+        }
+	}
+	
+	
+}
