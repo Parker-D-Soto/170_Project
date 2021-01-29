3f69246449dc19341d74428f6809af0ce2f48812
YongshiSun <yongshisun3@gmail.com>
2021-01-29 03:57:29 -0800

    scene recreated

--- 170_Project/Assets/Scripts/Player_Grapple.cs
+++ 170_Project/Assets/Scripts/Player_Grapple.cs
@@ -9,6 +9,9 @@ public class Player_Grapple : MonoBehaviour
     public float speed;
     private Rigidbody2D body;
     public Camera Camera;
+    bool damaged = false;
+    bool flash = true;
+    float timer = 3; //3 second damage state effect
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,7 @@ public class Player_Grapple : MonoBehaviour
 
         body.AddForce(movement * speed);
         var transform = this.GetComponent<Transform>();
+        var sprite = GetComponent<SpriteRenderer>();
 
         //get mouse input
         //Vector2 mouse_pos = Input.mousePosition;
@@ -54,6 +58,37 @@ public class Player_Grapple : MonoBehaviour
 
 
         }
+
+        //damage state effect
+        if(damaged){
+            //countdown for effect
+            if(timer >= 0){
+                timer -= Time.deltaTime;
+                Debug.Log("start");
+                if(flash){
+                    sprite.color = Color.grey;
+                    flash = false;
+                }
+                else{
+                    sprite.color = Color.white;
+                    flash = true;
+                }
+            }
+            else{
+                Debug.Log("Done");
+                sprite.color = Color.white;
+                damaged = false;
+                //reset timer to 3 second for next countdown
+                timer = 3;
+            }
+
+
+        
+            
+        }
+
+
+
     }
 
     //grappling effect
@@ -65,5 +100,12 @@ public class Player_Grapple : MonoBehaviour
         //Debug.Log("old pos: " + position);
         //Debug.Log("new pos: " + new_pos);
 
+    }
+
+    // if player touches enemy, then it goes into damage state
+    void OnCollisionEnter2D(Collision2D collider){
+        if(collider.gameObject.CompareTag("EnemyTag")){
+            damaged = true;
+        }
     }
 }
