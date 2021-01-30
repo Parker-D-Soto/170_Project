54d3e81bdd1b5d00dfb3671ce8474b5f34baf8cb
Parker-D-Soto <parkerdsoto@gmail.com>
2021-01-30 02:42:40 -0800

    Added Goblin Chace Attack

---
+++ 170_Project/Assets/Scripts/Chase_Behavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase_Behavior : MonoBehaviour
+{
+    //Skelly Transform (Contains location of G.0.)
+    public Transform skelly_player;
+    //Speed of goblin
+    public float speed;
+
+    //Actual vector locations of Skelly and the Goblin
+    private Vector2 skelly_loc;
+    private Vector2 goblin_loc;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        goblin_loc = gameObject.transform.position;
+        skelly_loc = skelly_player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float goblin_move = speed * Time.deltaTime;
+
+        //moving goblin towards player
+        transform.position = Vector2.MoveTowards(transform.position, skelly_loc, goblin_move);
+
+        skelly_loc = skelly_player.position;
+    }
+}
