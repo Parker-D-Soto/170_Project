a7a016d4479f2b0681222b2229e757a076523d69
HHUUUGE <63589860+HHUUUGE@users.noreply.github.com>
2021-02-12 03:10:03 -0800

    PuttingBossTogetherPrefabs Folder added with necessary prefabs
    Added PuttingBossTogetherPrefabs folder into prefabs folder with current necessary prefabs of the boss, player, projectile, and several goblin types for each of the boss' attacks.

--- 170_Project/Assets/Scripts/PuttingBossTogetherScripts/SteadyAimFireSpawn.cs
+++ 170_Project/Assets/Scripts/PuttingBossTogetherScripts/SteadyAimFireSpawn.cs
@@ -7,10 +7,10 @@ public class SteadyAimFireSpawn : MonoBehaviour
 {
     public GameObject fireSquad;
     public int gobbosInFireSquad = 3;
-    public Transform playerPosition;
 
     public void SpawnFireSquadNearPlayer()
     {
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
         List <GameObject> spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint").ToList<GameObject>();
         for (int i = 0; i < gobbosInFireSquad; i++)
         {
@@ -19,7 +19,7 @@ public class SteadyAimFireSpawn : MonoBehaviour
 
             foreach (GameObject spawnPoint in spawnPoints)
             {
-                float distance = Vector2.Distance(spawnPoint.transform.position,playerPosition.position);
+                float distance = Vector2.Distance(spawnPoint.transform.position,playerPosition);
                 if(distance < shortestDistance)
                 {
                     closest = spawnPoint;
