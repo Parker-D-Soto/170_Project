b25593e81d60548bbd80cdd7f11fb2c979153b3b
Parker-D-Soto <parkerdsoto@gmail.com>
2021-02-17 00:07:13 -0800

    Goblin Boss Scene Added and Working

---
+++ 170_Project/Assets/Scripts/BossDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDamage : MonoBehaviour
+{
+    public GameObject boss;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.tag == "clone")
+        {
+            Debug.Log("boss takes damage");
+            Destroy(hitInfo.gameObject);
+        }
+    }
+}
