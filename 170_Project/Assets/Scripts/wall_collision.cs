7d154a62cbe92b489e91cbb9962a2c80e429bb7f
YongshiSun <yongshisun3@gmail.com>
2021-02-17 23:02:12 -0800

    wall now works with kinematic body type

---
+++ 170_Project/Assets/Scripts/wall_collision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall_collision : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collider){
+        // if player touches wall, then body type = dynamic;
+        if(collider.gameObject.CompareTag("Wall")){
+            //body.isKinematic=false
+            body.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collider){
+        // if player stop touching wall, then body type = kinetic;
+        if(collider.gameObject.CompareTag("Wall")){
+            //body.isKinematic=false
+            body.bodyType = RigidbodyType2D.Kinematic;
+        }
+    }
+}
