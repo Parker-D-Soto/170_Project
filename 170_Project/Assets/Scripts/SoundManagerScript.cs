4baffb4bac7356b714f8e265f5c2a689c6f08b88
YongshiSun <yongshisun3@gmail.com>
2021-02-26 04:24:57 -0800

    coded SoundManagerScript

--- 170_Project/Assets/Scripts/SoundManagerScript.cs
+++ 170_Project/Assets/Scripts/SoundManagerScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SoundManagerScript : MonoBehaviour
 {
+    public static AudioClip DialogueBG, FightBG;
+    static AudioSource audioSrc;
     // Start is called before the first frame update
     void Start()
     {
+        //loading sounds from sound folder
+        FightBG = Resources.Load<AudioClip> ("oldversion-beat6");
+        DialogueBG = Resources.Load<AudioClip> ("beat3");
         
+        audioSrc = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static void PlaySound (string clip){
+        switch(clip){
+            case "Fight":
+                audioSrc.PlayOneShot(FightBG);
+                break;
+            case "Dialogue":
+                audioSrc.PlayOneShot(DialogueBG);
+                break;
+        }
     }
 }
