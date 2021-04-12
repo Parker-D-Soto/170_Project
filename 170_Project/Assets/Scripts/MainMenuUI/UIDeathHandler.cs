7c50f980b77e3fd95f542f2d9c279389dcbad8dd
jluo42 <jluo42@ucsc.edu>
2021-04-12 13:22:35 -0700

    adding UI menu/end screens

---
+++ 170_Project/Assets/Scripts/MainMenuUI/UIDeathHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIDeathHandler : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject boss;
+    public GameObject uiWinText;
+    public GameObject scenePortal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(boss == null) {
+            uiWinText.SetActive(true);
+            scenePortal.SetActive(true);
+            
+        } 
+        else {
+            uiWinText.SetActive(false);
+            scenePortal.SetActive(false);
+        }
+
+        if(player == null) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); // move to main menu
+        }
+    }
+
+    
+}
