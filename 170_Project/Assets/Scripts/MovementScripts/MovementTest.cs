caa573036e9a5160b2ba93635fbfdb95287cfa74
Parker-D-Soto <parkerdsoto@gmail.com>
2021-04-19 02:49:37 -0700

    Skelly Walk F and Walk D Anim

--- 170_Project/Assets/Scripts/MovementScripts/MovementTest.cs
+++ 170_Project/Assets/Scripts/MovementScripts/MovementTest.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MovementTest : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public Animator anim;
 
     public Rigidbody2D rb;
     public Vector2 movement;
@@ -16,7 +17,7 @@ public class MovementTest : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-
+        anim.SetFloat("vertSpeed", movement.y);
     }
 
     void FixedUpdate()
@@ -25,6 +26,7 @@ public class MovementTest : MonoBehaviour
         if (!gameObject.GetComponent<Updated_Player_Stats>().Check_Dialogue_Status() && !gameObject.GetComponent<Updated_Player_Stats>().Check_Grapple_Status() && !gameObject.GetComponent<Updated_Player_Stats>().Check_Dash_Status())
         {
             rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+            anim.SetFloat("vertSpeed", movement.y);
         }
 
     }
