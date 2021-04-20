bd2e428049aa62462f075b43baaa8100df812dd1
HHUUUGE <63589860+HHUUUGE@users.noreply.github.com>
2021-04-20 13:01:28 -0700

    Added arrow rotation at spawn and fixed player tracking for spawn glitch

--- 170_Project/Assets/Scripts/GoblinBossAI/SteadyAimFire/enemyFire.cs
+++ 170_Project/Assets/Scripts/GoblinBossAI/SteadyAimFire/enemyFire.cs
@@ -9,15 +9,18 @@ public class enemyFire : MonoBehaviour
 
     public GameObject projectile;
     private Transform player;
-
+    private Quaternion rotation;
 	void Start() {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         timeBtwShots = startTimeBtwShots;
+        Vector2 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 	
 	void Update() {
         if (timeBtwShots <= 0){
-             Instantiate(projectile, transform.position, Quaternion.identity);
+             Instantiate(projectile, transform.position, rotation);
              timeBtwShots = startTimeBtwShots;
         } else {
             timeBtwShots -= Time.deltaTime;
