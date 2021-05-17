d7159eee4b60124d9eda0803af9f6a4f5325727c
YongshiSun <yongshisun3@gmail.com>
2021-05-17 10:37:27 -0700

    volume panel in scene and gobbo health reduced

--- 170_Project/Assets/Scripts/GoblinBossAI/EnemyHealth.cs
+++ 170_Project/Assets/Scripts/GoblinBossAI/EnemyHealth.cs
@@ -6,6 +6,7 @@ public class EnemyHealth : MonoBehaviour
 {
     public GameObject bar;
     public float health;
+    public int type; // 1 = firegobbo  2 = getemgobob
     
 
     // Start is called before the first frame update
@@ -24,16 +25,22 @@ public class EnemyHealth : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.gameObject.layer == LayerMask.NameToLayer("Pickaxe")){
             if(GameObject.Find("Player UM").GetComponent<GrappleProj>().holdobject == false){ //accessing holdobject from GrappleProj script
-                Debug.Log("colliding with crystal");
-                health = health - 0.2f;
-                if(health == 0){
+                if(type == 1){
+                    Debug.Log("firegobbo collided with crystal");
                     Object.Destroy(this.gameObject);
                 }else{
-                    //bar.transform.localScale = new Vector3(health/3.0f, 1.0f, 1.0f);
-                    bar.transform.localScale = new Vector3(180.0f*health, 150.0f, 1.0f);
-                    Debug.Log("enemy got hit");
-                    Debug.Log(bar.transform.localScale);
+                    Debug.Log("getemgobbo collided with crystal");
+                    health = health - 0.4f;
+                    if(health <= 0){
+                        Object.Destroy(this.gameObject);
+                    }else{
+                        //bar.transform.localScale = new Vector3(health/3.0f, 1.0f, 1.0f);
+                        bar.transform.localScale = new Vector3(180.0f*health, 150.0f, 1.0f);
+                        Debug.Log("enemy got hit");
+                        Debug.Log(bar.transform.localScale);
+                    }
                 }
+                
             }
         }
     
