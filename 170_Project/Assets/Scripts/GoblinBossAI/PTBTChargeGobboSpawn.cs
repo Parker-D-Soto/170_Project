064616b476708f9697dd3678ef3a60f17e400cde
HHUUUGE <63589860+HHUUUGE@users.noreply.github.com>
2021-06-04 02:25:48 -0700

    Added red eyes that appear 0.5s before goblins charge out of caves.

--- 170_Project/Assets/Scripts/GoblinBossAI/PTBTChargeGobboSpawn.cs
+++ 170_Project/Assets/Scripts/GoblinBossAI/PTBTChargeGobboSpawn.cs
@@ -9,7 +9,9 @@ public class PTBTChargeGobboSpawn : MonoBehaviour
     public GameObject fireSquad;
     public int gobbosInFireSquad = 3;
     public Transform player;
-
+    public GameObject eyes;
+    Vector3[] positions;
+    GameObject[] eyeLocations;
     public void SpawnChargeGobboNearPlayer()
     {
         //GameObject.Find("Boss_Attack_Canvas/Next_Attack").GetComponent<Text>().text = "Charge the enemy";
@@ -18,6 +20,8 @@ public class PTBTChargeGobboSpawn : MonoBehaviour
         if(player != null)
         {
             Vector3 playerPosition = player.position;
+            positions = new Vector3[gobbosInFireSquad];
+            eyeLocations = new GameObject[gobbosInFireSquad];
             List<GameObject> spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint").ToList<GameObject>();
             for (int i = 0; i < gobbosInFireSquad; i++)
             {
@@ -33,13 +37,52 @@ public class PTBTChargeGobboSpawn : MonoBehaviour
                         shortestDistance = distance;
                     }
                 }
-
-                Instantiate(fireSquad, closest.transform.position, Quaternion.identity);
+                if(closest.transform.position.y >= 303)
+                {
+                    eyeLocations[i] = Instantiate(eyes, closest.transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    eyeLocations[i] = Instantiate(eyes, closest.transform.position, Quaternion.AngleAxis(90, new Vector3(0, 0, 1)));
+                }
+                positions[i] = closest.transform.position;
                 spawnPoints.Remove(closest);
             }
+
+            Invoke("Spawnner", 0.5f);
         }
         
     }
+
+
+    //WILL PROBABLY CHANGE THIS WHEN NEW SPRITES ARE IN
+    private void Spawnner()
+    {
+        foreach (Vector3 point in positions)
+        {
+            foreach (GameObject eye in eyeLocations)
+            {
+                Destroy(eye);
+            }
+
+            if(point.y >= 303)
+            {
+                Instantiate(fireSquad, point, Quaternion.identity);
+                
+            }
+            else if(point.x < 0)
+            {
+                Instantiate(fireSquad, point, Quaternion.AngleAxis(90, new Vector3(0, 0, 1)));
+            }
+            else
+            {
+                Instantiate(fireSquad, point, Quaternion.AngleAxis(90, new Vector3(0, 0, 1)));
+            }
+            
+        }
+        
+    }
+
     //dies with one shot if hit by player
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.gameObject.tag == "Player"){
